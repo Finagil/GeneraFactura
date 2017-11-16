0566e7540084bacb2d6a1b360e6b19e8f3f21a96
ecaceres <ecacerest@finagil.com.mx>
2017-11-15 19:42:41 -0600

    Initial commit

---
+++ ProductionDataSet.vb
@@ -0,0 +1,12 @@
+﻿Partial Class ProductionDataSet
+    Partial Class FacturasAvioDataTable
+
+        Private Sub FacturasAvioDataTable_FacturasAvioRowChanging(ByVal sender As System.Object, ByVal e As FacturasAvioRowChangeEvent) Handles Me.FacturasAvioRowChanging
+
+        End Sub
+
+    End Class
+
+End Class
+
+
